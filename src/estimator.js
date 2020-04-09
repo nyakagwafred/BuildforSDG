056f293259f698473e8927c291fe2f252386ac0f
Fred Nyakagwa <51993500+nyakagwafred@users.noreply.github.com>
2020-04-09 16:52:50 +0300

    modified:   src/estimator.js - All three challanges complete

--- src/estimator.js
+++ src/estimator.js
@@ -9,42 +9,70 @@ const data = {
     avgDailyIncomePopulation: 0.71
   },
   periodType: 'days',
-  timeToElapse: 58,
+  timeToElapse: 6,
   reportedCases: 674,
   population: 66622705,
   totalHospitalBeds: 1380614
 }
 
-//Best case estimation
+//Best Case Estimation
 let impact = {
-    currentlyInfected: '',
+  currentlyInfected: '',
+  infectionsByRequestedTime: '',
+  severeCasesByRequestedTime: '',
+  hospitalBedsByRequestedTime: 0,
+  casesForICUByRequestedTime: 0,
+  casesForVentilatorsByRequestedTime: 0,
+  dollarsInFlight: ''
 };
   
 
 //Severe Case Estimation
 let severeImpact = {
-    currentlyInfected: '',
+  currentlyInfected: '',
+  infectionsByRequestedTime:'',
+  severeCasesByRequestedTime: '',
+  hospitalBedsByRequestedTime: 0,
+  casesForICUByRequestedTime: 0,
+  casesForVentilatorsByRequestedTime: 0,
+  dollarsInFlight: ''
 };
 
 
 //Impact Estimator Function
 const covid19ImpactEstimator = (data) => {
 
- impact.currentlyInfected = data.reportedCases * 10;
- severeImpact.currentlyInfected = data.reportedCases * 50;
+//Challenge 1
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  impact.infectionsByRequestedTime = Math.round(impact.currentlyInfected * (2 ** (data.timeToElapse / 3)));
+  severeImpact.infectionsByRequestedTime = Math.round(severeImpact.currentlyInfected * (2 ** (data.timeToElapse / 3)));
+
+//Challenge 2
+
+  impact.severeCasesByRequestedTime = Math.round(impact.infectionsByRequestedTime * 0.15);
+  severeImpact.severeCasesByRequestedTime = Math.round(severeImpact.infectionsByRequestedTime * 0.15);
+
+  impact.hospitalBedsByRequestedTime = Math.round(((0.35 * data.totalHospitalBeds) - impact.severeCasesByRequestedTime));
+  severeImpact.hospitalBedsByRequestedTime = Math.round(((0.35 * data.totalHospitalBeds) - severeImpact.severeCasesByRequestedTime));
+
+//Challenge 3
+
+  impact.casesForICUByRequestedTime = Math.round(0.05 * impact.infectionsByRequestedTime);
+  severeImpact.casesForICUByRequestedTime = Math.round(0.05 * severeImpact.infectionsByRequestedTime);
+
+  impact.casesForVentilatorsByRequestedTime = Math.round(0.02 * impact.infectionsByRequestedTime);
+  severeImpact.casesForVentilatorsByRequestedTime = Math.round(0.02 * severeImpact.infectionsByRequestedTime);
+
+  impact.dollarsInFlight = Math.round((impact.infectionsByRequestedTime * 0.65) * 1.5 * data.timeToElapse);
+  severeImpact.dollarsInFlight = Math.round((severeImpact.infectionsByRequestedTime * 0.65) * 1.5 * data.timeToElapse);
 
 
  
- return { data, impact, severeImpact};
+  return { data, impact, severeImpact};
 };
 
 
-
-
-
-
-
-console.log(covid19ImpactEstimator(data));
-
-
-//export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
