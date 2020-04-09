45451250aebad59a5385c4d893f4abcca5bae507
Fred Nyakagwa <51993500+nyakagwafred@users.noreply.github.com>
2020-04-09 23:19:00 +0300

    Another commit

--- src/estimator.js
+++ src/estimator.js
@@ -30,20 +30,20 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
   
-  impact.infectionsByRequestedTime = Math.round(impact.currentlyInfected * (2 ** Math.floor((convertToDays(data) / 3))));
-  severeImpact.infectionsByRequestedTime = Math.round(severeImpact.currentlyInfected * (2 ** Math.floor((convertToDays(data) / 3))));
+  impact.infectionsByRequestedTime = (impact.currentlyInfected * (2 ** Math.floor((convertToDays(data) / 3))));
+  severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected * (2 ** Math.floor((convertToDays(data) / 3))));
 
-  impact.severeCasesByRequestedTime = Math.round(impact.infectionsByRequestedTime * 0.15);
-  severeImpact.severeCasesByRequestedTime = Math.round(severeImpact.infectionsByRequestedTime * 0.15);
+  impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.15);
+  severeImpact.severeCasesByRequestedTime = Math.floor(severeImpact.infectionsByRequestedTime * 0.15);
 
-  impact.hospitalBedsByRequestedTime = Math.round(((0.35 * data.totalHospitalBeds) - impact.severeCasesByRequestedTime));
-  severeImpact.hospitalBedsByRequestedTime = Math.round(((0.35 * data.totalHospitalBeds) - severeImpact.severeCasesByRequestedTime));
+  impact.hospitalBedsByRequestedTime = Math.floor(((0.35 * data.totalHospitalBeds) - impact.severeCasesByRequestedTime));
+  severeImpact.hospitalBedsByRequestedTime = Math.floor(((0.35 * data.totalHospitalBeds) - severeImpact.severeCasesByRequestedTime));
 
-  impact.casesForICUByRequestedTime = Math.round(0.05 * impact.infectionsByRequestedTime);
-  severeImpact.casesForICUByRequestedTime = Math.round(0.05 * severeImpact.infectionsByRequestedTime);
+  impact.casesForICUByRequestedTime = Math.floor(0.05 * impact.infectionsByRequestedTime);
+  severeImpact.casesForICUByRequestedTime = Math.floor(0.05 * severeImpact.infectionsByRequestedTime);
 
-  impact.casesForVentilatorsByRequestedTime = Math.round(0.02 * impact.infectionsByRequestedTime);
-  severeImpact.casesForVentilatorsByRequestedTime = Math.round(0.02 * severeImpact.infectionsByRequestedTime);
+  impact.casesForVentilatorsByRequestedTime = Math.floor(0.02 * impact.infectionsByRequestedTime);
+  severeImpact.casesForVentilatorsByRequestedTime = Math.floor(0.02 * severeImpact.infectionsByRequestedTime);
 
   const impactDollar = (impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation) * data.region.avgDailyIncomeInUSD * convertToDays(data);
   const severeDollar = (severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation) * data.region.avgDailyIncomeInUSD * convertToDays(data);
