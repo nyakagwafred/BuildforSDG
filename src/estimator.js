0dc07b370961bd89d157d2a90e81b9c990aa962e
Fred Nyakagwa <51993500+nyakagwafred@users.noreply.github.com>
2020-04-09 17:31:39 +0300

    modified:   src/estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,7 +1,5 @@
-/* eslint-disable spaced-comment */
-/* eslint-disable no-unused-vars */
-//Input Data
-const data = {
+
+const input = {
   region: {
     name: 'Africa',
     avgAge: 19.7,
@@ -41,34 +39,29 @@ const severeImpact = {
 
 //Impact Estimator Function
 const covid19ImpactEstimator = (data) => {
-  //Challenge 1
+
+const data = input;
 
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
-
+  
   impact.infectionsByRequestedTime = Math.round(impact.currentlyInfected * (2 ** (data.timeToElapse / 3)));
   severeImpact.infectionsByRequestedTime = Math.round(severeImpact.currentlyInfected * (2 ** (data.timeToElapse / 3)));
-
-  //Challenge 2
-
+  
   impact.severeCasesByRequestedTime = Math.round(impact.infectionsByRequestedTime * 0.15);
   severeImpact.severeCasesByRequestedTime = Math.round(severeImpact.infectionsByRequestedTime * 0.15);
-
+  
   impact.hospitalBedsByRequestedTime = Math.round(((0.35 * data.totalHospitalBeds) - impact.severeCasesByRequestedTime));
   severeImpact.hospitalBedsByRequestedTime = Math.round(((0.35 * data.totalHospitalBeds) - severeImpact.severeCasesByRequestedTime));
-
-  //Challenge 3
-
+  
   impact.casesForICUByRequestedTime = Math.round(0.05 * impact.infectionsByRequestedTime);
   severeImpact.casesForICUByRequestedTime = Math.round(0.05 * severeImpact.infectionsByRequestedTime);
-
+  
   impact.casesForVentilatorsByRequestedTime = Math.round(0.02 * impact.infectionsByRequestedTime);
   severeImpact.casesForVentilatorsByRequestedTime = Math.round(0.02 * severeImpact.infectionsByRequestedTime);
-
+  
   impact.dollarsInFlight = Math.round((impact.infectionsByRequestedTime * 0.65) * 1.5 * data.timeToElapse);
   severeImpact.dollarsInFlight = Math.round((severeImpact.infectionsByRequestedTime * 0.65) * 1.5 * data.timeToElapse);
-
-
   return { data, impact, severeImpact };
 };
 export default covid19ImpactEstimator;
